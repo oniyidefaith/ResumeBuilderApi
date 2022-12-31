85c9e686650fcb1b1cf1fca7f50429d4a4174ad6
Oniyide Faith <72167184+oniyidefaith@users.noreply.github.com>
2022-12-31 03:02:05 +0100

    prod

--- resumebuilder/settings.py
+++ resumebuilder/settings.py
@@ -237,6 +237,5 @@ CSRF_COOKIE_SECURE = True
 
 
 # Update database configuration from $DATABASE_URL.
-
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
