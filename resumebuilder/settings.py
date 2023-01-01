af6ae5e4095d10be05dd495b5e415975ae41d5c0
Oniyide Faith <72167184+oniyidefaith@users.noreply.github.com>
2023-01-02 00:08:05 +0100

    new

--- resumebuilder/settings.py
+++ resumebuilder/settings.py
@@ -235,10 +235,5 @@ JAZZMIN_UI_TWEAKS = {
 
 CSRF_COOKIE_SECURE = True
 
-
-# Update database configuration from $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
 django_heroku.settings(locals())
 
